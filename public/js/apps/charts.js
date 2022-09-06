46cb84079dfe3df1120373d9695dc997868a5a99
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2022-09-06 18:29:58 +0200

    Updated content

--- public/js/apps/charts.js
+++ public/js/apps/charts.js
@@ -1,109 +1,107 @@
-var labelsChart1 = ['3-Zimmer 1999', '3-Zimmer 2020']
+var labelsChart1 = ["3-Zimmer 1999", "3-Zimmer 2020"];
 
 var dataChart1 = {
-    labels: labelsChart1,
-    datasets: [{
-        label: "Monatsmiete 3-Zimmer Wohnung",
-        backgroundColor: 'rgba(126, 0, 0, 0.5)',
-        data: [1338, 2473]
-    }]
-}
+  labels: labelsChart1,
+  datasets: [
+    {
+      label: "Monatsmiete 3-Zimmer Wohnung",
+      backgroundColor: "rgba(126, 0, 0, 0.5)",
+      data: [1342, 1970],
+    },
+  ],
+};
 
 var configChart1 = {
-    type: 'bar',
-    data: dataChart1,
-    options: {
-        aspectRatio: (3/2),
-        scales: {
-            y: {
-                min: 0,
-                max: 3000,
-                ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                        return value + " CHF";
-                    }
-                }
-            }
+  type: "bar",
+  data: dataChart1,
+  options: {
+    aspectRatio: 3 / 2,
+    scales: {
+      y: {
+        min: 0,
+        max: 3000,
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return value + " CHF";
+          },
         },
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    }
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
 };
 
-var chart1 = new Chart(
-    document.getElementById('chart1'),
-    configChart1
-);
-
-
+var chart1 = new Chart(document.getElementById("chart1"), configChart1);
 
 var labelsChart2 = [
-    '2010',
-    '2011',
-    '2012',
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-    '2017',
-    '2018',
-    '2019',
-    '2020'
-]
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+];
 
 var dataChart2 = {
-    labels: labelsChart2,
-    datasets: [
-        {
-            label: "Im Besitz von Immobilienkonzernen (%)",
-            backgroundColor: 'rgba(31, 31, 143, 0.5)',
-            borderColor: 'rgba(31, 31, 143, 1)',
-            data: [25.5275387, 25.8293048, 26.54325356, 27.24225074, 27.68740979, 27.97846435, 29.15980905, 30.18146313, 30.79354088, 31.11307763, 31.78435169]
-        },
-        {
-            label: "Im Besitz der öffentlichen Hand (%)",
-            backgroundColor: 'rgba(197, 30, 30, 0.5)',
-            borderColor: 'rgba(197, 30, 30, 1)',
-            data: [25.14837401, 25.12466985, 25.04399187, 25.09977992, 24.83142052, 24.88615362, 24.49238463, 24.49684813, 24.53283265, 24.86659975, 24.83818947]
-        },
-    ]
-}
-
-var configChart2 = {
-    type: 'line',
-    data: dataChart2,
-    options: {
-        aspectRatio: (3/2),
-        scales: {
-            y: {
-                ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                        return value + "%";
-                    }
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    }
+  labels: labelsChart2,
+  datasets: [
+    {
+      label: "Im Besitz von Immobilienkonzernen (%)",
+      backgroundColor: "rgba(31, 31, 143, 0.5)",
+      borderColor: "rgba(31, 31, 143, 1)",
+      data: [
+        25.5275387, 25.8293048, 26.54325356, 27.24225074, 27.68740979,
+        27.97846435, 29.15980905, 30.18146313, 30.79354088, 31.11307763,
+        31.78435169,
+      ],
+    },
+    {
+      label: "Im Besitz der öffentlichen Hand (%)",
+      backgroundColor: "rgba(197, 30, 30, 0.5)",
+      borderColor: "rgba(197, 30, 30, 1)",
+      data: [
+        25.14837401, 25.12466985, 25.04399187, 25.09977992, 24.83142052,
+        24.88615362, 24.49238463, 24.49684813, 24.53283265, 24.86659975,
+        24.83818947,
+      ],
+    },
+  ],
 };
 
-var chart2 = new Chart(
-    document.getElementById('chart2'),
-    configChart2
-);
+var configChart2 = {
+  type: "line",
+  data: dataChart2,
+  options: {
+    aspectRatio: 3 / 2,
+    scales: {
+      y: {
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return value + "%";
+          },
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+};
 
-
-
-
+var chart2 = new Chart(document.getElementById("chart2"), configChart2);
 
 // var labelsChart3 = [
 //     '2010',
@@ -155,138 +153,140 @@ var chart2 = new Chart(
 // }
 
 var labelsChart3 = [
-    '2009',
-    '2010',
-    '2011',
-    '2012',
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-    '2017',
-    '2018',
-    '2019',
-    '2020',
-    '2021'
-]
+  "2009",
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019",
+  "2020",
+  "2021",
+];
 
 var dataChart3 = {
-    labels: labelsChart3,
-    datasets: [
-        {
-            label: "Allreal",
-            backgroundColor: 'rgba(31, 31, 143, 0.5)',
-            borderColor: 'rgba(31, 31, 143, 1)',
-            data: [100,101.27,111.38,112.58,103.64,101.93,106.32,117.95,139.98,130.3,155.1,152.17,156.72],
-            tension: 0.3
-        },
-        {
-            label: "PSP",
-            backgroundColor: 'rgba(255, 0, 0, 0.5)',
-            borderColor: 'rgba(255, 0, 0, 1)',
-            data: [100,118.14,136.89,152.62,138.61,142.39,143.94,161.99,151.33,165.61,226.14,185.55,211.18],
-            tension: 0.3
-        },
-        {
-            label: "mobimo",
-            backgroundColor: 'rgba(128, 0, 128, 0.5)',
-            borderColor: 'rgba(128, 0, 128, 1)',
-            data: [100,115.1,144.51,145.83,128.78,124.55,132.15,162.09,175.35,159.65,185.43,175.51,213.53],
-            tension: 0.3
-        },
-    ]
-}
+  labels: labelsChart3,
+  datasets: [
+    {
+      label: "Allreal",
+      backgroundColor: "rgba(31, 31, 143, 0.5)",
+      borderColor: "rgba(31, 31, 143, 1)",
+      data: [
+        100, 101.27, 111.38, 112.58, 103.64, 101.93, 106.32, 117.95, 139.98,
+        130.3, 155.1, 152.17, 156.72,
+      ],
+      tension: 0.3,
+    },
+    {
+      label: "PSP",
+      backgroundColor: "rgba(255, 0, 0, 0.5)",
+      borderColor: "rgba(255, 0, 0, 1)",
+      data: [
+        100, 118.14, 136.89, 152.62, 138.61, 142.39, 143.94, 161.99, 151.33,
+        165.61, 226.14, 185.55, 211.18,
+      ],
+      tension: 0.3,
+    },
+    {
+      label: "mobimo",
+      backgroundColor: "rgba(128, 0, 128, 0.5)",
+      borderColor: "rgba(128, 0, 128, 1)",
+      data: [
+        100, 115.1, 144.51, 145.83, 128.78, 124.55, 132.15, 162.09, 175.35,
+        159.65, 185.43, 175.51, 213.53,
+      ],
+      tension: 0.3,
+    },
+  ],
+};
 
 var configChart3 = {
-    type: 'line',
-    data: dataChart3,
-    options: {
-        aspectRatio: (3/2),
-        scales: {
-            y: {
-                ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                        return value + "%";
-                    }
-                }
-            }
+  type: "line",
+  data: dataChart3,
+  options: {
+    aspectRatio: 3 / 2,
+    scales: {
+      y: {
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return value + "%";
+          },
         },
-    }
+      },
+    },
+  },
 };
 
-var chart3 = new Chart(
-    document.getElementById('chart3'),
-    configChart3
-);
+var chart3 = new Chart(document.getElementById("chart3"), configChart3);
 
-var labelsChart4 = ['2-Zimmer', '3-Zimmer', '4-Zimmer']
+var labelsChart4 = ["2-Zimmer", "3-Zimmer", "4-Zimmer"];
 
 var dataChart4 = {
-    labels: labelsChart4,
-    datasets: [
-        {
-            label: "Gemeinnützig",
-            backgroundColor: 'rgba(197, 30, 30, 0.5)',
-            borderColor: 'rgba(197, 30, 30, 1)',
-            data: [915,1016,1400]
-        }, {
-            label: "Renditeorientiert",
-            backgroundColor: 'rgba(31, 31, 143, 0.5)',
-            borderColor: 'rgba(31, 31, 143, 1)',
-            data: [1341,1586,2000]
-        }
-    ]
-}
-
-var configChart4 = {
-    type: 'bar',
-    data: dataChart4,
-    options: {
-        aspectRatio: (3/2),
-        scales: {
-            y: {
-                // display: false,
-                ticks: {
-                    display: false
-                }
-            }
-        }
-    }
+  labels: labelsChart4,
+  datasets: [
+    {
+      label: "Gemeinnützig",
+      backgroundColor: "rgba(197, 30, 30, 0.5)",
+      borderColor: "rgba(197, 30, 30, 1)",
+      data: [915, 1016, 1400],
+    },
+    {
+      label: "Renditeorientiert",
+      backgroundColor: "rgba(31, 31, 143, 0.5)",
+      borderColor: "rgba(31, 31, 143, 1)",
+      data: [1341, 1586, 2000],
+    },
+  ],
 };
 
-var chart1 = new Chart(
-    document.getElementById('chart4'),
-    configChart4
-);
+var configChart4 = {
+  type: "bar",
+  data: dataChart4,
+  options: {
+    aspectRatio: 3 / 2,
+    scales: {
+      y: {
+        // display: false,
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+};
 
-
+var chart1 = new Chart(document.getElementById("chart4"), configChart4);
 
 // DOWNLOAD CHARTS
 
-$(".savechart").click(function() {
-
-    let name = $(this).attr("data-name")
-    $(`#${$(this).attr("data-ccid")} .hideonchart`).css("display", "none")
-    html2canvas(document.querySelector(`#${$(this).attr("data-ccid")}`)).then(function(canvas) {
-        saveAs(canvas.toDataURL(), `${name}.png`);
-    });
-    $(`#${$(this).attr("data-ccid")} .hideonchart`).css("display", "block")
+$(".savechart").click(function () {
+  let name = $(this).attr("data-name");
+  $(`#${$(this).attr("data-ccid")} .hideonchart`).css("display", "none");
+  html2canvas(document.querySelector(`#${$(this).attr("data-ccid")}`)).then(
+    function (canvas) {
+      saveAs(canvas.toDataURL(), `${name}.png`);
+    }
+  );
+  $(`#${$(this).attr("data-ccid")} .hideonchart`).css("display", "block");
 });
 
-
 function saveAs(uri, filename) {
-    var link = document.createElement('a');
-    if (typeof link.download === 'string') {
-        link.href = uri;
-        link.download = filename;
-        //Firefox requires the link to be in the body
-        document.body.appendChild(link);
-        //simulate click
-        link.click();
-        //remove the link when done
-        document.body.removeChild(link);
-    } else {
-        window.open(uri);
-    }
+  var link = document.createElement("a");
+  if (typeof link.download === "string") {
+    link.href = uri;
+    link.download = filename;
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+    //simulate click
+    link.click();
+    //remove the link when done
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
 }
--- templates/initiative/initiative.php
+++ templates/initiative/initiative.php
@@ -9,14 +9,14 @@ include __DIR__ . "/../partials/header.php";
 
 <div class="paper-container mdcont">
     <div class="paper">
-        <h2 class="paper-title">Initiative für eine Wohn-Offensive unterstützen</h2>
-        <p class="lead">Wir müssen gemeinsam dafür sorgen, dass nicht bald halb Zürich den Immobilienkonzernen gehört. Mit unserer neuen Initiative fordern wir: Mehr bezahlbare Wohnungen, weniger Immobilienkonzerne!</p>
-        
+        <h2 class="paper-title">Initiative für mehr bezahlbare WOhnungen im Kanton Zürich unterschreiben</h2>
+        <p class="lead">Wir müssen gemeinsam dafür sorgen, dass nicht bald der halbe Kanton Zürich den Immobilienkonzernen gehört. Mit unserer neuen Initiative fordern wir: Mehr bezahlbare Wohnungen, weniger Immobilienkonzerne!</p>
+
         <div class="testimonial">
             <img src="/img/personen/jacqueline_badran.jpeg" alt="Jacqueline Badran" class="testimonial-img">
             <div class="testimonial-quote-outer">
                 <p class="testimonial-quote">
-                    «9 von 10 Personen würden lieber in einer gemeinnützigen Wohnung zur Kostenmiete leben, statt einem Immobilienkonzern Monat für Monat die übersetzte Rendite zu finanzieren. Darum: <b>Unterstütze jetzt unsere Initiative!</b>»
+                    «9 von 10 Personen würden lieber in einer gemeinnützigen Wohnung zur Kostenmiete leben, statt einem Immobilienkonzern Monat für Monat die übersetzte Rendite zu finanzieren. Darum: <b>Unterschreib jetzt unsere Initiative!</b>»
                 </p>
                 <p class="testimonial-description">
                     <b>Jacqueline Badran</b><br>
@@ -24,27 +24,90 @@ include __DIR__ . "/../partials/header.php";
                 </p>
             </div>
         </div>
-        
+
         <div id="mitmachen" style="padding-top: calc(var(--navHeight) + 1.5rem)">
             <h2 style="color: var(--red); margin: 0; text-align: center">Hilf mit!</h2>
             <div data-prefill-inherit="1" data-paperform-id="yrhzauzy" class="paperform-container" style="margin-bottom: 0"></div><script>(function() {var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; document.body.appendChild(script); })()</script>
         </div>
-        
+
         <div id="darum-gehts" style="padding-top: calc(var(--navHeight) + 1.5rem)">
             <h4 style="margin-top: 0">Warum braucht es die Initiative?</h4>
-            <p>Die Mieten in der Stadt Zürich haben sich in den letzten 20 Jahren fast verdoppelt. Immobilienkonzerne und dubiose Immobilien-AGs bringen einen immer grösseren Anteil der Wohnungen in der Stadt Zürich in ihren Besitz und bereichern sich schamlos auf Kosten der Bevölkerung. Geht die Entwicklung weiter wie in den letzten 10 Jahren, besitzen Immobilienkonzerne bis 2050 jede zweite Mietwohnungen in der Stadt Zürich. Das müssen wir unbedingt verhindern!</p>
-        
-            <h4>Was fordert die Initiative der SP?</h4>
-            <p>Die Initiative fordert ein Massnahmenpaket, damit in den nächsten Jahren endlich mehr bezahlbare Wohnungen entstehen und die Ausbreitung der Immobilienkonzerne gestoppt werden kann.</p>
-            <p style="margin-bottom: 0.25rem"><b>Konkret fordert die Initiative:</b></p>
-            <ol>
-                <li>Der Stadtrat erhält den Auftrag, mehr Liegenschaften zu kaufen, anstatt sie den Immobilienkonzernen zu überlassen. Die Wohnungen können in der Folge zur Kostenmiete vermietet werden, damit die Mietenden – wie heute schon bei Genossenschaften – keine Konzern-Renditen bezahlen müssen und entsprechend von tieferen Mieten profitieren.</li>
-                <li>Die städtischen Stiftungen PWG (Stiftung zur Erhaltung von preisgünstigen Wohn- und Gewerberäumen), SAW (Stiftung Alterswohnungen) und die Stiftung "Wohnen für kinderreiche Familien" erhalten insgesamt 250 Mio. Franken frisches Eigenkapital. Damit können sie auch mehr bezahlbare Wohnungen realisieren.</li>
-                <li>Mit Darlehen für die städtischen Stiftungen und Bürgschaften für Genossenschaften können diese gemeinnützigen Wohnbauträger günstiger Geld aufnehmen und noch mehr bezahlbare bezahlbare Wohnungen realisieren.</li>
-                <li>Die Initiative fordert, dass die Stiftung SAW bis 2040 2'000 neue gemeinnützige Alterswohnungen realisieren muss.</li>
-                <li>Ein zusätzliches Augenmerk legt die Initiative auf Menschen in besonders prekären Lebenssituationen, die es auf dem Wohnungsmarkt speziell schwer haben. 5 % der neuen Wohnungen sollen deshalb gezielt und unkompliziert an sozial benachteiligte Mitmenschen vermietet werden.</li>
-            </ol>
-            <a href="#mitmachen" class="button">Jetzt mitmachen</a>
+            <p>
+                Wer heute im Kanton Zürich eine Wohnung sucht, zahlt rund 50 Prozent mehr als noch vor zwanzig Jahren. Das Problem beschränkt sich längst nicht auf die Grossstädte Zürich und Winterthur. Auch in vielen Agglomerationsgemeinden hat der Preisdruck in den letzten Jahren massiv zugenommen und es fehlt vielerorts an verfügbaren Landreserven, um bezahlbaren Wohnraum zu ermöglichen.</br>
+                Gleichzeitig kaufen grosse Immobilienfirmen sehr viel Land und realisieren häufig überteuerte Wohnungen. Diese Entwicklung müssen wir gemeinsam stoppen.
+            </p>
+        </div>
+
+        <div id="unsere-forderungen" style="padding-top: calc(var(--navHeight) + 1.5rem)">
+            <h4 style="margin-top: 0">Was fordert die Initiative?</h4>
+            <p>Mit der kantonalen Initiative «Mehr bezahlbare Wohnungen im Kanton Zürich» wird eine wichtige Grundlage geschaffen, damit Städte und Gemeinden einfacher an Land kommen, um bezahlbaren Wohnraum zu schaffen. Dank einem sogenannten «Vorkaufsrecht» sollen die Gemeinden bei Verkäufen von grossen Arealen jeweils entscheiden können, ob sie das zum Verkauf stehende Grundstück zum ausgehandelten Preis selbst erwerben möchten. So kann die Gemeinde mehr bezahlbare Wohnungen bereitstellen oder das Grundstück einer gemeinnützigen Wohnbaugenossenschaft weitergeben, die dort bezahlbare Wohnungen realisiert.</p>
+            <a href="#mitmachen" class="button">Jetzt unterschreiben</a>
+        </div>
+
+        <div id="unsere-argumente" style="padding-top: calc(var(--navHeight) + 1.5rem)">
+            <h4 style="margin-top: 0">Unsere Argumente</h4>
+            <ul>
+                <li>Die Mieten im Kanton Zürich steigen. <strong>Handeln wir nicht, gehört bald der halbe Kanton Immobilienkonzernen</strong>, die hohe Renditen verlangen. Die Initiative «Mehr bezahlbare Wohnungen im Kanton Zürich» gibt den Gemeinden ein neues Instrument, um diese Entwicklung zu stoppen und bezahlbare Wohnungen zu schaffen.</li>
+                <li>Das Vorkaufsrecht stellt sicher, dass die <strong>Gemeinden über Verkaufsabsichten überhaupt Bescheid wissen </strong>und verhindern können, dass Immobilienkonzerne alle Areale aufkaufen. So können die Gemeinden ihren boden- und wohnungspolitischen Spielraum erweitern.</li>
+                <li><strong>Das Vorkaufsrecht ist bereits in mehreren Kantonen ein bewährtes Instrument!</strong> Zum Beispiel im Kanton Waadt, wo die Gemeinden seit Januar 2020 vom Vorkaufsrecht Gebrauch machen können. So konnte die Stadt Lausanne schon mehrere Liegenschaften mit insgesamt 78 Wohnungen kaufen.</li>
+                <li>Die <strong>Nachfrage seitens der Städte und Gemeinden ist da</strong>: Laut einer Umfrage des Städteverbandes stuften rund zwei Drittel der Antwortenden ein Vorkaufsrecht für Gemeinden als “eher oder sehr zweckmässig” ein.</li>
+                <li><strong>Ein Vorkaufsrecht für Gemeinden ist finanzierbar</strong> Dazu gibt es verschiedene Möglichkeiten: zum Beispiel könnte die Exekutive die Budgetkompetenzen für den Landerwerb erhalten, über die Gründung von privatrechtlichen Trägerschaften, oder mithilfe eines Fonds zur Finanzierung der Boden- und Liegenschaftspolitik, der unter anderem auch von Geldern des Mehrwertausgleichs gespiesen wird.</li>
+            </ul>
+            <a href="#mitmachen" class="button">Jetzt unterschreiben</a>
+        </div>
+
+        <div id="initiative" style="padding-top: calc(var(--navHeight) + 1.5rem)">
+            <h4 style="margin-top: 0">Initiativtext</h4>
+            <div id="initiativtext" style="margin-bottom: 1rem;">
+                <p id="initiativtext-title">Kantonale Volksinitiative «Mehr bezahlbare Wohnungen im Kanton Zürich»</p>
+                <p id="initiativtext-subtitle">Kommunales Vorkaufsrecht</p>
+                <div class="kapitel">
+                    <p class="kapitel-title">a. Allgemeines</p>
+                    <div class="artikel">
+                        <p class="artikel-no">§ 14 b.</p>
+                        <p class="absatz"><sup>1</sup>Zur Förderung des gemeinnützigen und preisgünstigen Wohnens sowie der Bereitstellung von Alterswohnungen können die Gemeinden in einem Gemeindeerlass ein Vorkaufsrecht an Grundstücken vorsehen und alle erforderlichen Bestimmungen erlassen.</p>
+                        <p class="absatz"><sup>2</sup>Das Vorkaufsrecht kann für Eigentumsübertragungen von Grundstücken in Bauzonen vorgesehen werden, die mehrheitlich der Wohnnutzung dienen oder auf denen eine mehrheitliche Wohnnutzung planungs- und baurechtlich zulässig ist.</p>
+                        <p class="absatz"><sup>3</sup>Es geht vertraglichen Vorkaufsrechten vor und ist zum gesetzlichen Vorkaufsrecht gemäss Art. 682 ZGB subsidiär.</p>
+                    </div>
+                </div>
+                <div class="kapitel">
+                    <p class="kapitel-title">b. Ausnahmen</p>
+                    <div class="artikel">
+                        <p class="artikel-no">§ 14 c.</p>
+                        <p class="absatz"><sup>1</sup>Vom Vorkaufsrecht ausgenommen sind Eigentumsübertragungen</p>
+                            <p class="litera">a. zwischen Familienangehörigen, die in gerader Linie oder in der Seitenlinie bis und mit dem vierten Grad verwandt oder verschwägert sind,</p>
+                            <p class="litera">b. wegen Eigenbedarfs der erwerbenden Person,</p>
+                            <p class="litera">c. an gemeinnützige Wohnbauträger.</p>
+                        <p class="absatz"><sup>2</sup>Die Gemeinden können weitere Ausnahmen vorsehen, insbesondere können sie Grundstücke bis zu einer bestimmten Grösse vom Vorkaufsrecht ausnehmen.</p>
+                    </div>
+                </div>
+                <div class="kapitel">
+                    <p class="kapitel-title">c. Ausübungspreis und Aufwendungsersatz</p>
+                    <div class="artikel">
+                        <p class="artikel-no">§ 14 d.</p>
+                        <p class="absatz"><sup>1</sup>Die Gemeinde kann das Vorkaufsrecht zu den Bedingungen der erwerbenden Person ausüben.</p>
+                        <p class="absatz"><sup>2</sup>Sie vergütet den Parteien die angemessenen Aufwendungen und aufgelaufenen Zinsen.</p>
+                    </div>
+                </div>
+                <div class="kapitel">
+                    <p class="kapitel-title">d. Verfahren</p>
+                    <div class="artikel">
+                        <p class="artikel-no">§ 14 e.</p>
+                        <p class="absatz"><sup>1</sup>Lässt sich ein Vorkaufsfall nicht ausschliessen, trägt das Grundbuchamt die Grundbuchanmeldung in das Tagebuch ein und sistiert das weitere Eintragungsverfahren. Es stellt der Gemeinde die Grundbuchanmeldung und den Rechtsgrundausweis zu.</p>
+                        <p class="absatz"><sup>2</sup>Beabsichtigt die Gemeinde, das Vorkaufsrecht auszuüben, setzt der Gemeindevorstand die Parteien und das Grundbuchamt innert einer Verwirkungsfrist von 60 Tagen schriftlich und unter Angabe der Gründe davon in Kenntnis. Zugleich setzt der Gemeindevorstand den Parteien eine Frist von 20 Tagen zur Stellungnahme. Unterbleibt die Absichtserklärung, kann der Verkauf in das Hauptbuch eingetragen werden.</p>
+                        <p class="absatz"><sup>3</sup>Innert einer Ordnungsfrist von 60 Tagen ab Eingang der Stellungnahmen kann der Gemeindevorstand das Vorkaufsrecht mit schriftlichem und begründetem Entscheid definitiv ausüben.</p>
+                        <p class="absatz"><sup>4</sup>Eine allfällige Ausgabenbewilligung durch ein anderes Organ ist vorbehalten und kann nach Ablauf der Frist erfolgen.</p>
+                    </div>
+                </div>
+                <div class="kapitel">
+                    <p class="kapitel-title">e. Rechtsschutz</p>
+                    <div class="artikel">
+                        <p class="artikel-no">§ 14 f.</p>
+                        <p class="absatz">Der Entscheid des Gemeindevorstands kann mit Rekurs beim Baurekursgericht angefochten werden.</p>
+                    </div>
+                </div>
+            </div>
+            <a href="#mitmachen" class="button">Jetzt unterschreiben</a>
         </div>
     </div>
 </div>
